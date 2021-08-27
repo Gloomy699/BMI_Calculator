d3bb14a95eb84e780e053d103b34265f4a8e33ab
Yaroslav Vasylenko <gloomy.699@gmail.com>
2021-08-27 17:24:59 +0300

    complete App skeleton

---
+++ lib/reusebable_card.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ReusebableCard extends StatelessWidget {
+  ReusebableCard({@required this.colour});
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
